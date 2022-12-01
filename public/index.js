c08c63d3d045f3b9c7e9107581ed96408c38e4bc
mn-spawn <madelinespawn@gmail.com>
2022-12-01 04:09:55 -0800

    added the start of a modal
    -Added start of modal
    -created file system for handlebars
    -Made the page look nicer
    -fixed some json and wrote some js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,14 @@
-/* Espresso and Filter Page */
-function drinkClick(){
+function drinkClick(name, type){
     /*send name to drop downs? */
+    console.log(name)
+    console.log(type)
     window.open("recipe.html","_self")
 }
+
+let cancelButton = document.getElementById('modal-cancel')
+let closeButton = document.getElementById('modal-close')
+cancelButton.addEventListener("click", () => {
+    modal.classList.add('hidden')
+    modalBackdrop.classList.add('hidden')
+});
+

b327d272e10af78e9ed4d5875d3cdc4fc3b4b328
mn-spawn <madelinespawn@gmail.com>
2022-12-04 04:39:47 -0800

    Added js stuff
    -Made whole divs clickable and routes to maker
    - Made it so you can switch back and forth between espresso and filter select using buttons
    -Modal opens on button click
    -Espresso/Filter buttons are underlined on click

--- public/index.js
+++ public/index.js
@@ -1,14 +1,37 @@
-function drinkClick(name, type){
-    /*send name to drop downs? */
-    console.log(name)
-    console.log(type)
-    window.open("recipe.html","_self")
+var drinks = document.getElementsByClassName("drink")
+
+
+for(let i = 0; i < drinks.length; i++){
+    drinks[i].onclick= function(){window.open("/recipe.html", "_self")}
 }
 
-let cancelButton = document.getElementById('modal-cancel')
-let closeButton = document.getElementById('modal-close')
-cancelButton.addEventListener("click", () => {
-    modal.classList.add('hidden')
-    modalBackdrop.classList.add('hidden')
-});
+
+var beanbutton = document.getElementById("bean-button")
+beanbutton.addEventListener("click", function(){
+    document.getElementById("modal-backdrop").classList.remove("hidden")
+    document.getElementById("bean-counter-modal").classList.remove("hidden")
+})
+
+var modalclose = document.getElementById("modal-close")
+modalclose.addEventListener("click", function(){
+    document.getElementById("modal-backdrop").classList.add("hidden")
+    document.getElementById("bean-counter-modal").classList.add("hidden")
+})
+
+var espressobutton = document.getElementById('espresso')
+var filterbutton = document.getElementById('filter')
+espressobutton.addEventListener("click", function(){
+    espressobutton.style.textDecoration = "underline"
+    filterbutton.style.textDecoration = "none"
+    document.getElementById("espressoSelector").classList.remove("hidden")
+    document.getElementById("filterSelector").classList.add("hidden")
+})
+filterbutton.addEventListener("click", function(){
+    filterbutton.style.textDecoration = "underline"
+    espressobutton.style.textDecoration = "none"
+    document.getElementById("filterSelector").classList.remove("hidden")
+    document.getElementById("espressoSelector").classList.add("hidden")
+})
+
+
 

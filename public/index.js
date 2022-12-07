bcee9eed44a06bcc8ca2e7c6bd4dfe3c50f27dc5
Leon Ong <ongl@oregonstate.edu>
2022-12-06 17:56:20 -0800

    new

--- public/index.js
+++ public/index.js
@@ -1,4 +1,37 @@
-function clickForRecipe(){
-    console.log("clicked div")
+var drinks = document.getElementsByClassName("drink")
 
+
+for(let i = 0; i < drinks.length; i++){
+    drinks[i].onclick= function(){window.open("/recipe.html", "_self")}
 }
+
+
+var beanbutton = document.getElementById("bean-button")
+beanbutton.addEventListener("click", function(){
+    document.getElementById("modal-backdrop").classList.remove("hidden")
+    document.getElementById("bean-counter-modal").classList.remove("hidden")
+})
+
+var modalclose = document.getElementById("modal-close")
+modalclose.addEventListener("click", function(){
+    document.getElementById("modal-backdrop").classList.add("hidden")
+    document.getElementById("bean-counter-modal").classList.add("hidden")
+})
+
+var espressobutton = document.getElementById('espresso')
+var filterbutton = document.getElementById('filter')
+espressobutton.addEventListener("click", function(){
+    espressobutton.style.textDecoration = "underline"
+    filterbutton.style.textDecoration = "none"
+    document.getElementById("espressoSelector").classList.remove("hidden")
+    document.getElementById("filterSelector").classList.add("hidden")
+})
+filterbutton.addEventListener("click", function(){
+    filterbutton.style.textDecoration = "underline"
+    espressobutton.style.textDecoration = "none"
+    document.getElementById("filterSelector").classList.remove("hidden")
+    document.getElementById("espressoSelector").classList.add("hidden")
+})
+
+
+
--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 var path = require('path');
 var express = require('express');
 var expressbars = require('express-handlebars');
+var recipeData = require('./recipeData.json');
+const { resourceUsage } = require('process');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
 app.set('view engine', 'handlebars')
 app.use(express.static('public'));
 
+app.engine('handlebars', expressbars.engine({
+  defaultLayout: "main"
+}))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'));
 
-  app.use(express.static('public'));
-
-app.get('/', function (req, res, next) {
+  app.get('/', function (req, res, next) {
     res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
     next()
   });
 
-  app.get('/:drink', function (req, res, next) {
-    console.log(req.params.drink)
-    next()
-  });
-
   app.get('*', function (req, res) {
-    res.status(200).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
   })
 
 
 app.listen(port, function () {
     console.log("Coffee Companion server is open on:", port);
   });
+
+
+//this is the way to access for the handlebar
+/* console.log(recipeData.espresso['Affogato']) */
